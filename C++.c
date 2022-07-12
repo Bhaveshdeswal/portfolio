e13600d459304458f7f2c09dfaabe1d290f9461c
Bhaveshdeswal <106268802+Bhaveshdeswal@users.noreply.github.com>
2022-07-12 10:36:25 +0530

    Add files via upload

---
+++ C++.c
@@ -0,0 +1,11 @@
+void main()
+{
+    clrscr();
+    int a,b;
+    printf("Enter the first number:");
+    scanf("%d",&a);
+    printf("Enter the second number:");
+    scanf("%d",&b);
+    printf("The sum of given numbers is %d",a+b);
+    getch();
+}
